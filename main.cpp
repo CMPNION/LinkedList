0e77a8ac5e8906f87def70cef927c7e99e786042
CMPNION <norkindima57@gmail.com>
2025-02-23 23:42:33 +0500

    get size and overrided operator[]

--- main.cpp
+++ main.cpp
@@ -5,15 +5,26 @@ struct ListItem {
     ListItem *next;
 };
 
-class List {
+class List  {
 private:
     ListItem *head = nullptr;
     ListItem *tail = nullptr;
 
-public:
-    [[nodiscard]] bool isEmpty() const { return head == nullptr; }
 
-    void append(int value) {
+public:
+
+
+    [[nodiscard]] bool isEmpty() const { return head == nullptr; }
+    [[nodiscard]] int size() const {
+        const ListItem *current = head;
+        int size = 1;
+        while (current->next != nullptr) {
+            current = current->next;
+            size++;
+        }
+        return size;
+    }
+    void append(const int value) {
         auto *node = new ListItem;
         node->value = value;
         node->next = nullptr;
@@ -26,7 +37,7 @@ public:
             tail = node;
         }
     }
-    void insert(int value) {
+    void insert(const int value) {
         auto *node = new ListItem;
         node->value = value;
         node->next = head;
@@ -40,14 +51,33 @@ public:
         }
     }
 
-
-    void print_all() {
+    void print() const {
         ListItem *current = head;
+        std::cout << "Head -> ";
         while (current != nullptr) {
             printf("%d ", current->value);
             current = current->next;
         }
         printf("\n");
+    }
+
+    int operator[](int index) const {
+        ListItem *current = head;
+
+        if (index > size()-1 || index < 0) {
+            throw std::out_of_range("Index is out of range");
+        }
+        try {
+            int i = 0;
+            while (i < index) {
+                current = current->next;
+                i++;
+            }
+            return current->value;
+        }
+        catch (...){
+            throw std::out_of_range("Index is out of range");
+        }
     }
 
     ~List() {
@@ -62,16 +92,23 @@ public:
 
 int main() {
     List list;
-    std::cout<<std::to_string(list.isEmpty());
+    std::cout << "Is empty: " << list.isEmpty() << "\n";
+
     list.append(10);
     list.append(20);
     list.append(30);
 
-    list.print_all();
+    list.print();
+    std::cout << "Is empty: " << list.isEmpty() << "\n";
+    std::cout << "Size:" << list.size() << "\n";
 
     list.insert(40);
-    std::cout<<std::to_string(list.isEmpty());
-    list.print_all();
+    list.print();
+    std::cout << "Size:" << list.size() << "\n";
 
+    std::cout << "Element of list[0]: " << list[0] << "\n";
+    std::cout << "Element of list[1]: " << list[1] << "\n";
+    std::cout << "Element of list[2]: " << list[2] << "\n";
+    std::cout << "Element of list[3]: " << list[3] << "\n";
     return 0;
 }
